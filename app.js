971b75efbe14dcea9b57dac828fed2f968581fc3
KUMAR SAURAV <kumarsauravsingh1234567890@gmail.com>
2023-01-03 23:19:31 +0530

    in progress

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const mongoConnect = require('./database/db_connection.js')
+
+const app = express();
+
+
+
+app.listen(3000,function(){
+    console.log("Server Running");
+});
+
---
+++ database/db_connection.js
@@ -0,0 +1,21 @@
+// CONNECT MONGO DATABASE
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(process.env.DB_CONNECT,{
+    useNewUrlParser: true,
+},(err)=>{
+    if(err)
+        console.log("Database is not connected", err);
+    else   
+        console.log("Database connected");
+});
+
+// module.exports = mongoConnect;
+
+
+
