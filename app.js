fde0eb726bc0f48fb78b5320354f146a29f2e382
KUMAR SAURAV <kumarsauravsingh1234567890@gmail.com>
2023-01-05 00:11:07 +0530

    login part

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoConnect = require('./database/db_connection.js')
+const user = require('./models/userProfile');
 
 const app = express();
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+app.use(bodyParser.json())
+
 const apis = require('./backend/apis.js')(app);
 
 
--- backend/apis.js
+++ backend/apis.js
@@ -1,15 +1,75 @@
+const user = require('../models/userProfile');
+
 const welcomePage = (req,res,next) => {
     console.log("I am at the login Page");
     res.send("Hello! Please wait. Its loading!")
 }
 
+const registerPage = (req,res,next) =>{
+    res.send("Welcome to the Register Page");
+}
+
+const signUp = (req,res,next) =>{
+
+    let username = req.body.username;
+    let password = req.body.password;
+
+    const newUser = new user({
+        username: username,
+        password: password
+    });
+
+    let query = {"username": username};
+
+    user.find(query, async function(err,result){
+        if(err)
+            throw err;
+        else    
+        {
+            
+            if(result.length>0)
+                res.send("User exists!");
+            else
+            {
+                await newUser.save();
+                res.send("User Registerd");
+            }
+        }
+    })
+
+}
+
+const userLogin = (req,res,next) => {
+
+    let username = req.body.username;
+    let password = req.body.password;
+
+    let query = {username: username, password: password};
+    user.find(query,(err,result)=>{
+        if(err)
+            throw err;
+        else
+        {
+            if(result.length>0)
+                res.send("Welcome to the login Page");
+            else    
+                res.send("User doesnot exist. Please register!")
+        }        
+    })
+}
+
+const homePage = (req,res,next) =>{
+    res.send("You are in the home Page now"); 
+}
+
+
 module.exports = function(app){
 
     app.get('/',welcomePage);
-    // app.get('/register', registerPage);
-    // app.post('/register', signUp);
-    // app.post('/login', userLogin);
-    // app.get('/home', homePage);
+    app.get('/register', registerPage);
+    app.post('/register', signUp);
+    app.post('/login', userLogin);
+    app.get('/home', homePage);
 
 }
 
